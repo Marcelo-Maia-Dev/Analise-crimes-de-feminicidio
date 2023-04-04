2c3a90a5a7758d3e0ebd0ae9e077dea15b161f69
Marcelo Maia <marcelo.maia962@gmail.com>
2023-04-04 19:44:38 -0300

    Importando as bibliotecas e a base de dados

---
+++ analise-de-dados.py
@@ -0,0 +1,18 @@
+# Desafio 
+
+# Ver o que tem na base de dados
+
+# Importando bibliotecas e lendo o arquivo em csv
+import pandas as pd  
+
+tabela = pd.read_excel("Feminicidio_2015_2022.xlsx")
+
+display(tabela)
+
+# Panorama geral da base de dados
+
+
+# Começar a analise top => down 
+
+
+# Entrar no detalhe para entender
