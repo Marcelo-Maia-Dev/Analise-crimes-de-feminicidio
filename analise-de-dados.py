886436780d84f5a34754d6f1844b2f68d65f9253
Marcelo Maia <marcelo.maia962@gmail.com>
2023-04-05 21:35:36 -0300

    Atualizando....M

--- analise-de-dados.py
+++ analise-de-dados.py
@@ -4,13 +4,17 @@
 
 # Importando bibliotecas e lendo o arquivo em csv
 import pandas as pd  
+import numpy as np
+import matplotlib.piplot as plt
+import seaborn as sns
 
-tabela = pd.read_excel("Feminicidio_2015_2022.xlsx")
 
-display(tabela)
+df = pd.read_excel("Feminicidio_2015_2022.xlsx")
+
+print(df)
 
 # Panorama geral da base de dados
-
+total_morte = pd[df].groupby("MUNICIPIO_CIRCUNSCRICAO").sum()
 
 # Começar a analise top => down 
 
